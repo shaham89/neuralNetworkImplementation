b7fb56433426e89eb770076cb9eb4cd06396e19d
shaham89 <shmerom99@gmail.com>
2023-07-04 19:27:14 +0300

    saving

--- Layer.py
+++ Layer.py
@@ -4,4 +4,8 @@ from NetworkNode import *
 class Layer:
     DEFAULT_LAYER = np.array([2, 1])
 
-    def __init__(self, X, y,)
+    def __init__(self, nodes_array):
+        self.m_nodes_array = nodes_array
+
+
+
